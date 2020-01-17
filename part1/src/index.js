8950222a5822677f6abb6fb56fb9e89975843d7c
Adebola <onedebos@gmail.com>
2020-01-17 12:53:01 +0100

    complete ex1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,30 +1,59 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const App = () => {
-  const course = "Half stack application development";
-  const part1 = "Fundamentals of React";
+  const course = 'Half stack application development';
+  const part1 = 'Fundamentals of React';
   const exercises1 = 10;
-  const part2 = "using props to pass data";
+  const part2 = 'using props to pass data';
   const exercises2 = 7;
-  const part3 = "state of a component";
+  const part3 = 'state of a component';
   const exercises3 = 14;
+
+  App.propTypes = {
+    course: PropTypes.string,
+    sum: PropTypes.number,
+    part: PropTypes.string,
+    exercises: PropTypes.string,
+    text: PropTypes.string,
+  };
+
+  App.defaultProps = {
+    course: '',
+    sum: 0,
+    part: '',
+    exercises: '',
+    text: '',
+  };
+
+  const Header = ({ course }) => (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  );
+
+  const Content = () => (
+    <div>
+      <p>{`${part1} ${exercises1}`}</p>
+      <p>{`${part2} ${exercises2}`}</p>
+      <p>{`${part3} ${exercises3}`}</p>
+    </div>
+  );
+
+  const Total = ({ sum, text }) => (
+    <div>
+      <p>{text + sum}</p>
+    </div>
+  );
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content />
+      <Total sum={exercises1 + exercises2 + exercises3} text="Number of exercises " />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
