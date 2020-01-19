1d66cb65b182b263d011a02b90b0f5d3acfb162d
Adebola <onedebos@gmail.com>
2020-01-19 01:58:11 +0100

    refactoring code for Buttons

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,23 +5,43 @@ import PropTypes from 'prop-types';
 const Titles = ({ title }) => <h1>{title}</h1>;
 
 const Statistic = ({ text, counter, symbol }) => (
-  <div>
+  <td>
     {`${text} ${counter}`}
     {symbol === undefined ? '' : `${symbol}`}
-  </div>
+  </td>
 );
 const Statistics = ({ counter }) => {
   if (counter.total === 0) {
-    return <div>No feedback available</div>;
+    return (
+      <div>
+        <h1>Statistics</h1>
+        No feedback available
+      </div>
+    );
   }
   return (
     <div>
-      <Statistic text="good" counter={counter.good} />
-      <Statistic text="neutral" counter={counter.neutral} />
-      <Statistic text="bad" counter={counter.bad} />
-      <Statistic text="Totals: " counter={counter.total} />
-      <Statistic text="Average: " counter={counter.average} />
-      <Statistic text="Positive: " counter={counter.positive} symbol="%" />
+      <h1>Statistics</h1>
+      <tbody>
+        <tr>
+          <Statistic text="good" counter={counter.good} />
+        </tr>
+        <tr>
+          <Statistic text="neutral" counter={counter.neutral} />
+        </tr>
+        <tr>
+          <Statistic text="bad" counter={counter.bad} />
+        </tr>
+        <tr>
+          <Statistic text="Totals: " counter={counter.total} />
+        </tr>
+        <tr>
+          <Statistic text="Average: " counter={counter.average} />
+        </tr>
+        <tr>
+          <Statistic text="Positive: " counter={counter.positive} symbol="%" />
+        </tr>
+      </tbody>
     </div>
   );
 };
@@ -31,6 +51,10 @@ const Button = ({ buttonName, whenClicked }) => (
     {buttonName}
   </button>
 );
+
+const Buttons = ({ runFeed }) => {
+  <Button buttonName="good" whenClicked={runFeed.handleGood} />;
+};
 
 Titles.propTypes = {
   title: PropTypes.string,
@@ -68,56 +92,57 @@ const App = () => {
     positive: 0,
   });
 
-  const handleGood = () => {
-    setCalculator({
-      ...calculate,
-      total: calculate.good + calculate.bad + calculate.neutral + 1,
-      good: calculate.good + 1,
-      positive: ((calculate.good + 1) / (calculate.total + 1)) * 100,
-      average: (calculate.good + 1 - calculate.bad) / (calculate.total + 1),
-    });
-  };
+  const buttonObj = () => {
+    const handleGood = () => {
+      setCalculator({
+        ...calculate,
+        total: calculate.good + calculate.bad + calculate.neutral + 1,
+        good: calculate.good + 1,
+        positive: ((calculate.good + 1) / (calculate.total + 1)) * 100,
+        average: (calculate.good + 1 - calculate.bad) / (calculate.total + 1),
+      });
+    };
 
-  const handleBad = () => {
-    setCalculator({
-      ...calculate,
+    const handleBad = () => {
+      setCalculator({
+        ...calculate,
 
-      total: calculate.good + calculate.bad + calculate.neutral + 1,
-      bad: calculate.bad + 1,
-      positive: (calculate.good / (calculate.total + 1)) * 100,
-      average: (calculate.good - (calculate.bad + 1)) / (calculate.total + 1),
-    });
-  };
-  const handleNeutral = () => {
-    setCalculator({
-      ...calculate,
-      total: calculate.good + calculate.bad + calculate.neutral + 1,
-      neutral: calculate.neutral + 1,
-      positive: (calculate.good / (calculate.total + 1)) * 100,
-      average: (calculate.good - calculate.bad) / (calculate.total + 1),
-    });
-  };
+        total: calculate.good + calculate.bad + calculate.neutral + 1,
+        bad: calculate.bad + 1,
+        positive: (calculate.good / (calculate.total + 1)) * 100,
+        average: (calculate.good - (calculate.bad + 1)) / (calculate.total + 1),
+      });
+    };
+    const handleNeutral = () => {
+      setCalculator({
+        ...calculate,
+        total: calculate.good + calculate.bad + calculate.neutral + 1,
+        neutral: calculate.neutral + 1,
+        positive: (calculate.good / (calculate.total + 1)) * 100,
+        average: (calculate.good - calculate.bad) / (calculate.total + 1),
+      });
+    };
 
-  const reset = () => {
-    setCalculator({
-      ...calculate,
-      positive: 0,
-      total: 0,
-      neutral: 0,
-      average: 0,
-      bad: 0,
-      good: 0,
-    });
+    const reset = () => {
+      setCalculator({
+        ...calculate,
+        positive: 0,
+        total: 0,
+        neutral: 0,
+        average: 0,
+        bad: 0,
+        good: 0,
+      });
+    };
+
+    return { reset, handleBad, handleGood, handleNeutral };
   };
 
   return (
     <div>
       <Titles title="give feedback" />
-      <Button buttonName="good" whenClicked={handleGood} />
-      <Button buttonName="bad" whenClicked={handleBad} />
-      <Button buttonName="reset" whenClicked={reset} />
-      <Button buttonName="neutral" whenClicked={handleNeutral} />
-      <Titles title="statistics" />
+
+      <Buttons whenClicked={buttonObj} />
       <Statistics counter={calculate} />
     </div>
   );
