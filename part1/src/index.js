1c4eaeac8cbd3071f4054ca8aec6ae21a23d039b
Adebola <onedebos@gmail.com>
2020-01-19 13:10:16 +0100

    Implement anecdotes with most votes ex1.14

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-const Anecdote = ({ anecdote, num }) => <div>{anecdote[num]}</div>;
-const Button = ({ btnName, onClick }) => (
+
+const Anecdote = ({ anecdote, num, votes, title }) => (
   <div>
-    <button onClick={onClick}>{btnName}</button>
+    <h1>{title}</h1>
+    {anecdote[num]}
+    <p>Has {votes[num]} votes </p>
   </div>
+);
+const Button = ({ btnName, onClick }) => (
+  <button type="button" onClick={onClick}>
+    {btnName}
+  </button>
 );
 
 const App = () => {
   const [selected, getSelected] = useState(0);
-  const [points, getPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 });
+  const [points, setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 });
+  const [maxVotesVal, setMaxVotes] = useState(0);
 
   const anecdotes = [
     'If it hurts, do it more often',
@@ -22,22 +30,54 @@ const App = () => {
   ];
 
   const getRandomNum = () => {
-    getSelected(Math.floor(Math.random() * 5));
-    console.log(selected);
+    getSelected(Math.floor(Math.random() * 6));
   };
 
   const voteForAnecdote = () => {
-    if (getRandomNum === 1) {
-      points(getPoints({ 1: 1 }));
+    switch (selected) {
+      case 0:
+        setPoints({ ...points, 0: points['0'] + 1 });
+        break;
+      case 1:
+        setPoints({ ...points, 1: points['1'] + 1 });
+        break;
+      case 2:
+        setPoints({ ...points, 2: points['2'] + 1 });
+        break;
+      case 3:
+        setPoints({ ...points, 3: points['3'] + 1 });
+        break;
+      case 4:
+        setPoints({ ...points, 4: points['4'] + 1 });
+        break;
+      case 5:
+        setPoints({ ...points, 5: points['5'] + 1 });
+        break;
+      default:
+        setPoints({ ...points });
     }
-    console.log(points);
+    maxVotes();
+  };
+
+  const getKeyByValue = (obj, val) => Object.keys(obj).find((key) => obj[key] === val);
+
+  const maxVotes = () => {
+    const arr = Object.values(points);
+    const max = Math.max(...arr);
+    setMaxVotes(getKeyByValue(points, max));
   };
 
   return (
     <div>
-      <Anecdote anecdote={anecdotes} num={selected} />
+      <Anecdote anecdote={anecdotes} num={selected} votes={points} title="Anecdote of the day" />
       <Button btnName="next anecdote" onClick={getRandomNum} />
       <Button btnName="vote" onClick={voteForAnecdote} />
+      <Anecdote
+        anecdote={anecdotes}
+        num={maxVotesVal}
+        votes={points}
+        title="Anecdote with most votes"
+      />
     </div>
   );
 };
