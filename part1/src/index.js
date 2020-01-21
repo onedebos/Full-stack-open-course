5e80479a63aa3e72e9bb8d14ceec34beaf28cccb
Adebola <onedebos@gmail.com>
2020-01-21 01:40:05 +0100

    Ex2.1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,53 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Course = ({ course, header, part, exercises }) => (
+  <div>
+    <Header course={header} />
+    <Content part={part} exercises={exercises} />
+    {course}
+  </div>
+);
+const Header = ({ course }) => (
+  <div>
+    <h1>{course}</h1>
+  </div>
+);
+
+const Content = ({ part, exercises }) => (
+  <div>
+    <p>{`${part} ${exercises}`}</p>
+  </div>
+);
+
+const Total = ({ sum, text }) => (
+  <div>
+    <p>{text + sum}</p>
+  </div>
+);
+
 const App = () => {
-  const course = {
-    name: 'Half stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'state of a component',
-        exercises: 14,
-      },
-    ],
-  };
+  const courses = [
+    {
+      title: 'Half stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'state of a component',
+          exercises: 14,
+          id: 3,
+        },
+      ],
+    },
+  ];
 
-  const Header = ({ course }) => (
-    <div>
-      <h1>{course}</h1>
-    </div>
-  );
-
-  const Content = ({ part, exercises }) => (
-    <div>
-      <p>{`${part} ${exercises}`}</p>
-    </div>
-  );
-
-  const Total = ({ sum, text }) => (
-    <div>
-      <p>{text + sum}</p>
-    </div>
-  );
-
+  const displayCourses = () =>
+    courses.map((course) => (
+      <div>
+        <Content
+          key={course.parts.map((part) => part.id)}
+          part={course.parts.map((part) => part.name)}
+          exercises={course.parts.map((part) => part.exercises)}
+        />
+      </div>
+    ));
   return (
     <div>
-      <Header course={course.name} />
-      <Content part={course.parts[0].name} exercises={course.parts[0].exercises} />
-      <Content part={course.parts[1].name} exercises={course.parts[1].exercises} />
-      <Content part={course.parts[2].name} exercises={course.parts[2].exercises} />
-      <Total
-        sum={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
-        text="Number of exercises "
-      />
+      <Course key="0" course={displayCourses()} />
     </div>
   );
 };
