f67320c8c4a1c1edf19295cda04c480d494a6bb3
Adebola <onedebos@gmail.com>
2020-01-24 22:30:38 +0100

    complete ex2 phonebook

--- part2/src/componentsEx2.6/App.js
+++ part2/src/componentsEx2.6/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './Filter';
 import Phonebook from './Phonebook';
 import Persons from './Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '39-44-231393' },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data));
+  };
+
+  useEffect(hook, []);
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter persons={persons} />
       <Phonebook persons={persons} />
-      <Persons persons={persons} />
     </div>
   );
 };
--- part2/src/index.js
+++ part2/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// import App from './components/App';
 import App from './componentsEx2.6/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
